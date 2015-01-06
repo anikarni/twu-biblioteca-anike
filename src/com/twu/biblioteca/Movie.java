c5f1553de70a58c543a7411e8902a1d140137fd3
Anike Arni <anikarni@gmail.com>
2015-01-06 14:15:51 -0200

    created movie class

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by aarni on 1/6/15.
+ */
+public class Movie {
+    String title;
+    int year;
+    String director;
+    String rating;
+
+    public Movie(String title, int year, String director, String rating){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    
+
+}
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by aarni on 1/6/15.
+ */
+public class MovieTest {
+
+}
