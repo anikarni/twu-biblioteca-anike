39d912176d92d95009b5ff375e70191ce4cbc245
Anike Arni <anikarni@gmail.com>
2015-01-14 15:36:00 -0200

    mocks classes in CheckoutOptionTest

--- test/com/twu/biblioteca/CheckoutOptionTest.java
+++ test/com/twu/biblioteca/CheckoutOptionTest.java
@@ -3,27 +3,31 @@ package com.twu.biblioteca;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by aarni on 1/8/15.
  */
+
 public class CheckoutOptionTest {
+    Library library = mock(Library.class);
+    User user = mock(User.class);
     Book book = new Book("Book Example", "Anike", 1991);
     Book[] books = {book};
-    Movie movie = new Movie("Title", 2000, "director", "2");
+    Movie movie = new Movie("Name", 2000, "director", "2");
     Movie[] movies = {movie};
-    Customer customer = new Customer("aarni", "123", "Anike", "aarni@example", "1234-123");
-    Librarian librarian = new Librarian("librarian", "23");
-    User[] users = {customer, librarian};
-    Library library = new Library(books, movies, users);
+    List<Book> bookList = new ArrayList<Book>();
+    List<Movie> movieList = new ArrayList<Movie>();
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
@@ -37,43 +41,39 @@ public class CheckoutOptionTest {
         System.setOut(null);
     }
 
-    public void loginCustomer(){
-        ByteArrayInputStream inContent = new ByteArrayInputStream("aarni\n123".getBytes());
-        System.setIn(inContent);
-        library.selectOption("Login");
-    }
-
     @Test
     public void checkoutBookSuccessfully(){
-        loginCustomer();
-        CheckoutOption option = new CheckoutOption(library, library.getBooks());
-        option.checkout("Book Example");
-        assertNotEquals(books, library.getAvailable(books));
+        bookList.add(book);
+        Mockito.doReturn(bookList).when(library).getAvailable(books);
+        when(library.getCurrentUser()).thenReturn(user);
+
+        CheckoutOption subject = new CheckoutOption(library, books);
+        subject.checkout("Book Example");
+
         assertThat(outContent.toString(), containsString("Thank you! Enjoy the book."));
     }
 
     @Test
     public void checkoutMovieSuccessfully(){
-        loginCustomer();
-        CheckoutOption option = new CheckoutOption(library, library.getMovies());
-        option.checkout("Title");
-        assertNotEquals(movies, library.getAvailable(movies));
+        movieList.add(movie);
+        Mockito.doReturn(movieList).when(library).getAvailable(movies);
+        when(library.getCurrentUser()).thenReturn(user);
+
+        CheckoutOption subject = new CheckoutOption(library, movies);
+        subject.checkout("Name");
+
         assertThat(outContent.toString(), containsString("Thank you! Enjoy the movie."));
     }
 
     @Test
     public void checkoutBookUnsuccessfully(){
-        loginCustomer();
-        CheckoutOption option = new CheckoutOption(library, library.getBooks());
-        option.checkout("dasda");
+        bookList.add(book);
+        Mockito.doReturn(bookList).when(library).getAvailable(books);
+
+        CheckoutOption subject = new CheckoutOption(library, books);
+        subject.checkout("Ddasddasda");
+
         assertThat(outContent.toString(), containsString("That item is not available."));
     }
 
-    @Test
-    public void checkoutMovieUnsuccessfully(){
-        loginCustomer();
-        CheckoutOption option = new CheckoutOption(library, library.getMovies());
-        option.checkout("Titledsa");
-        assertThat(outContent.toString(), containsString("That item is not available."));
-    }
 }
